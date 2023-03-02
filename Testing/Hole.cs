0ec1cb8b3a35b4db249a4b8e4a5fda5654c07fb6
William Hayes <wilthaye@ut.utm.edu>
2023-03-02 14:06:30 -0600

    Add project files.

---
+++ Testing/Hole.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Testing
+{
+    public class Hole : UIElement
+    {
+        int position;
+
+        public Hole() 
+        {
+        }
+        public void drawH(double X, double Y, Grid b)
+        {
+            Ellipse hole = new Ellipse
+            {
+                Tag = "H",
+                Height = 30,
+                Width = 30,
+                Fill = Brushes.Black,
+                
+            };
+
+            //hole.TranslatePoint(new Point(X, Y), b);
+
+            hole.Margin = new Thickness(X, Y, 0, 0);
+
+
+            b.Children.Add(hole);
+            b.UpdateLayout();
+
+        }
+
+
+    }
+
+}
---
+++ Testing/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Testing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            NewGame();
+        }
+
+        private void NewGame()
+        {
+            initializeHoles();
+
+            
+        }
+
+        private void initializeHoles()
+        {
+            Hole hole1 = new Hole();
+            Hole hole2 = new Hole();
+
+            hole1.drawH(10, 10, MainGrid);
+            hole2.drawH(50,100, MainGrid);
+
+        }
+
+    }
+}
